fcdb0dbf95176a847b6191c63af3a0b27c81bf65
Ferdinand Prantl <prantlf@gmail.com>
2019-08-18 14:45:04 +0200

    chore: Reduce code block nesting

--- index.js
+++ index.js
@@ -2,19 +2,20 @@ const { GraphQLObjectType } = require('graphql')
 
 function visitFieldsOfType (type, callback, visitedTypes) {
   const { name } = type
-  if (!visitedTypes.has(name)) {
-    visitedTypes.add(name)
-    const fields = type.getFields()
-    Object
-      .values(fields)
-      .forEach(field => {
-        const fieldType = field.type
-        if (fieldType instanceof GraphQLObjectType) {
-          visitFieldsOfType(fieldType, callback, visitedTypes)
-        }
-        callback(field, type)
-      })
+  if (visitedTypes.has(name)) {
+    return
   }
+  visitedTypes.add(name)
+  const fields = type.getFields()
+  Object
+    .values(fields)
+    .forEach(field => {
+      const fieldType = field.type
+      if (fieldType instanceof GraphQLObjectType) {
+        visitFieldsOfType(fieldType, callback, visitedTypes)
+      }
+      callback(field, type)
+    })
 }
 
 function visitFields (schema, callback) {
